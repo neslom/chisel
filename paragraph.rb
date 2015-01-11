8d9b1342805e3614f2c203d055573e1c00237c92
Markus Olsen <molsen13@gmail.com>
2015-01-11 12:04:23 -0700

    Add paragraph class

--- paragraph.rb
+++ paragraph.rb
@@ -1,3 +1,15 @@
 class Paragraph
+  attr_reader :text
+  def initialize(text)
+    @text = text
+  end
+
+  def paragraph_tagger
+   "<p>\n" + "  " + space_after_line_break + "\n</p>" 
+  end
+  
+  def space_after_line_break
+    text.gsub("\n","\n  ")
+  end
 
 end
--- paragraph_test.rb
+++ paragraph_test.rb
@@ -4,6 +4,24 @@ require_relative 'paragraph'
 
 class ParagraphTest < MiniTest::Test
   def test_it_exists
-    assert Paragraph.new
+    assert Paragraph.new("Hello")
+  end
+
+  def test_it_adds_tags_to_single_line_chunk
+    para = Paragraph.new("You just *have* to try the cheesecake,")
+    result = "<p>\n  You just *have* to try the cheesecake,\n</p>"
+    assert_equal result, para.paragraph_tagger
+  end
+
+  def test_it_adds_tags_to_multiple_line_chunk
+    para = Paragraph.new("You just *have* to try the cheesecake,\nit's so damn yummy.")
+    result = "<p>\n  You just *have* to try the cheesecake,\n  it's so damn yummy.\n</p>"                        
+    assert_equal result, para.paragraph_tagger
+  end
+
+  def test_spaces_after_line_break
+    para = Paragraph.new("Hey just checking\nto see if this works.")
+    result = "Hey just checking\n  to see if this works."
+    assert_equal result, para.space_after_line_break
   end
 end
